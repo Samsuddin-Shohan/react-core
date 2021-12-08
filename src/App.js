4714e7f9a8fc4ea47f8104bc7c2edc1f9a634f92
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2021-12-09 03:46:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const styleDiv = {
+    textAlign: 'center',
+  }
+  const players = [
+    {name: 'shohan', jersy: 10},
+    {name: 'jamal', jersy: 7},
+    {name: 'bitch', jersy: 13}
+
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {styleDiv}>
+      <Person name = 'shohan' age = '23'></Person>
+      <Person name = 'arif' age = '24'></Person>
+      {
+        players.map(player => <Player name = {player.name} jersy = {player.jersy}></Player> )
+      }    
     </div>
+    
   );
 }
 
+
+function Person(props) {
+  console.log(props);
+  return (
+    <div className = "person">
+      <h1>Name: {props.name}</h1>
+      <h3>age: {props.age}</h3>
+    </div>
+    
+  );
+}
+
+function Player(props){
+  console.log(props);
+  return(
+    <div className = "person">
+      <h1>Name: {props.name}</h1>
+      <h3>Jersy: {props.jersy}</h3>
+    </div>
+  );
+}
 export default App;
